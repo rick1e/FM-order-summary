783294d3cbd33c3d07f07641dc34d7f5247a6d6b
rick1e <alrickab@gmail.com>
2022-03-09 20:17:38 -0500

    Completed challenge + deployed to GH Pages

--- react-solution/src/App.js
+++ react-solution/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import hero from './assets/illustration-hero.svg'
+import music from './assets/icon-music.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div id="container">
+        <div id="hero">
+          <img src={hero} alt="HERO" />
+        </div>
+        <div id="summary-body">
+          <div id="summary-text">
+            <h2>Order Summary</h2>
+            <p>
+              You can now listen to millions of songs, audiobooks and podcasts on any device anywhere you like
+            </p>
+          </div>
+          <div id="order-item">
+            <div className="right-side">
+              <div className="music-icon">
+                <img src={music} alt="music" />
+              </div>
+              <div className="order-description">
+                <h3>Annual Plan</h3>
+                <p>$59.99/year</p>
+              </div>
+
+
+            </div>
+            <div className="left-side">
+              <a href="url">Change</a>
+            </div>
+
+          </div>
+
+          <button className="primaryButton">
+            Proceed to Payment
+        </button>
+          <button className="secondaryButton">
+            Cancel Order
+        </button>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
